bb3d52985db72a7a5bfc54321e1d3361ca70b059
Jeremias Agustinoy <48390333+JereA00@users.noreply.github.com>
2020-11-12 17:00:51 -0300

    Add files via upload

---
+++ sources_1/new/alu.v
@@ -0,0 +1,64 @@
+`timescale 1ns/1ps
+
+module alu
+        #(
+        parameter N_BITS_OP = 6,
+        parameter N_BITS = 8
+        )
+        
+        (
+            input wire [N_BITS_OP - 1 : 0]        i_operator,
+            input wire [N_BITS - 1 : 0]              i_data1,
+            input wire  [N_BITS - 1 : 0]             i_data2, 
+            output reg [N_BITS - 1 : 0]                o_alu
+        );
+                
+        /*!
+        *
+        *   +++++++++++++++++ Parameter Lists +++++++++++++++++
+        *
+        */
+        
+        parameter ADD_OP =  6'b100000;
+        parameter SUB_OP =  6'b100010;
+        parameter AND_OP =  6'b100100;
+        parameter OR_OP  =  6'b100101;
+        parameter XOR_OP =  6'b100110;
+        parameter SRA_OP =  6'b000011;
+        parameter SRL_OP =  6'b000010;
+        parameter NOR_OP =  6'b100111;
+        
+        always@(*)
+        begin
+                case (i_operator)
+                    ADD_OP: o_alu = i_data1 + i_data2;    //ADD
+                    SUB_OP: o_alu = i_data1 - i_data2;    //SUB 
+                    AND_OP: o_alu = i_data1 & i_data2;    //AND 
+                    OR_OP:  o_alu = i_data1 | i_data2;    //OR
+                    XOR_OP: o_alu = i_data1 ^ i_data2;    //XOR
+                    SRA_OP: 
+                    begin
+                        if (i_data2 > i_data1) begin
+                            o_alu = {N_BITS{1'b0}};
+                        end 
+                        else begin 
+                            o_alu = i_data1 >>> i_data2;  //SRA
+                        end 
+                    end
+                    SRL_OP: 
+                    begin
+                        if (i_data2 > i_data1) begin
+                            o_alu = {N_BITS{1'b0}};
+                        end 
+                        else begin 
+                            o_alu = i_data1 >> i_data2;   //SRL
+                        end 
+                    end 
+                    NOR_OP: o_alu = ~(i_data1 | i_data2); //NOR
+                    default  : 
+                    begin
+                        o_alu = {N_BITS{1'b0}};
+                    end
+                endcase
+        end
+ endmodule
---
+++ sources_1/new/interface_circuit.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+
+module interface_circuit
+#(
+	parameter  DBIT  = 8,  // buffer bits
+	           NB_OP = 6   // Operation bits
+	            
+) 
+(   //INPUT
+	input                     i_clk,
+ 	input                     i_reset,
+ 	input                     i_rx_done_tick,
+ 	input      [DBIT-1:0]     i_rx_data,
+ 	input      [DBIT-1:0]     i_alu_data_in,
+    //OUTPUT
+	output reg [DBIT-1 : 0]   o_data_a,
+	output reg [DBIT-1 : 0]   o_data_b,
+	output reg [NB_OP-1 : 0]  o_operation,
+ 	output reg                o_tx_start,
+ 	output     [DBIT-1:0]     o_data_out 
+);
+	
+	assign o_data_out = i_alu_data_in;
+	
+	reg [1 : 0] counter_in = 2'b00;
+	
+	
+	always @(posedge i_clk) 
+	begin
+        if (i_reset)  begin 
+            o_data_a      <= 0;
+            o_data_b      <= 0;
+            o_operation   <= 0;
+            counter_in    <= 0;
+            o_tx_start    <= 1'b0;
+        end 
+        else begin
+            if (i_rx_done_tick) begin
+                case (counter_in)
+                    2'b 00: o_data_a     = i_rx_data;
+                    2'b 01: o_data_b     = i_rx_data;
+                    2'b 10: o_operation  = i_rx_data;
+                endcase		
+                counter_in = counter_in + 1'b1;		
+            end
+            if (counter_in == 2'b11) begin //Si el contador llega a 11 vuelve al estado inicial i.e counter_in = 0 
+                counter_in = 0;	
+                o_tx_start   = 1'b1;
+            end else begin
+                o_tx_start = 1'b0;
+            end
+        end
+	end
+endmodule
